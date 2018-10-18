81400e82746f4fa1b5e22ba80d382905930b95de
RachMEow <31891237+RachMEow@users.noreply.github.com>
2018-10-17 22:52:45 -0400

    Register Interface

--- app/src/main/java/com/ondemandhomerepairservices/on_demandhomerepairservices/RegisterActivity.java
+++ app/src/main/java/com/ondemandhomerepairservices/on_demandhomerepairservices/RegisterActivity.java
@@ -2,12 +2,26 @@ package com.ondemandhomerepairservices.on_demandhomerepairservices;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    Spinner registerAs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        registerAs = findViewById(R.id.spinnerRegisterAs);
+        //create a list of items for the spinner.
+        String[] items = new String[]{"Admin", "Service Provider", "Home Owner"};
+        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
+        //There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        //set the spinners adapter to the previously created one.
+        registerAs.setAdapter(adapter);
+
     }
 }
