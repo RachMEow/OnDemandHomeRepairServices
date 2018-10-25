19d5b58346b86c140f147ea5051046117d13706f
Fluffy_Peppa <michelle1120lee@hotmail.com>
2018-10-24 20:47:56 -0400

    login admin can see list of users on the welcome page now.

--- app/src/main/java/com/ondemandhomerepairservices/on_demandhomerepairservices/LoginUser.java
+++ app/src/main/java/com/ondemandhomerepairservices/on_demandhomerepairservices/LoginUser.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.ListView;
 import android.widget.Button;
 import android.content.SharedPreferences;
 
