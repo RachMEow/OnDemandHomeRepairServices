d00af0033d0b15ba8b8bc763e836cb7d55e692c4
JiaruiSun <jiarui025@gmail.com>
2018-11-05 22:31:39 -0500

    Revert "login admin can see list of users on the welcome page now."
    This reverts commit 19d5b58346b86c140f147ea5051046117d13706f.

--- app/src/main/java/com/ondemandhomerepairservices/on_demandhomerepairservices/LoginUser.java
+++ app/src/main/java/com/ondemandhomerepairservices/on_demandhomerepairservices/LoginUser.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.ListView;
 import android.widget.Button;
 import android.content.SharedPreferences;
 
