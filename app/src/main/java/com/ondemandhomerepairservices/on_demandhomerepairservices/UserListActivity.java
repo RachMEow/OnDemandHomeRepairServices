2fefb79f22042d458e78f004202140e665366c3f
JiaruiSun <jiarui025@gmail.com>
2018-11-06 22:25:28 -0500

    ServiceList UI

--- app/src/main/java/com/ondemandhomerepairservices/on_demandhomerepairservices/UserListActivity.java
+++ app/src/main/java/com/ondemandhomerepairservices/on_demandhomerepairservices/UserListActivity.java
@@ -1,8 +1,6 @@
 package com.ondemandhomerepairservices.on_demandhomerepairservices;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,27 +12,27 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class LoginAdmin extends AppCompatActivity {
+public class UserListActivity extends AppCompatActivity {
 
-    Button buttonLogout;
-    TextView firstName;
+    Button buttonBack;
     ListView userList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_admin);
+        setContentView(R.layout.activity_user_list);
+
         userList = (ListView) findViewById(R.id._userlist);
 
-        buttonLogout = (Button) findViewById(R.id.buttonLogout);
-        firstName = (TextView) findViewById(R.id.textViewFirstName);
-        buttonLogout.setOnClickListener(new View.OnClickListener() {
+        buttonBack = (Button) findViewById(R.id.buttonLogout);
+        buttonBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(v.getContext(), MainActivity.class);
                 v.getContext().startActivity(intent);
             }
         });
+
         String[] formatUserList = getIntent().getStringExtra("USER_LIST").split("\t");
         List<String> your_array_list = new ArrayList<String>();
         for(int i=0; i<formatUserList.length; i++) {
@@ -42,6 +40,5 @@ public class LoginAdmin extends AppCompatActivity {
         }
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, your_array_list);
         userList.setAdapter(arrayAdapter);
-        firstName.setText(getIntent().getStringExtra("FIRST_NAME"));
     }
 }
