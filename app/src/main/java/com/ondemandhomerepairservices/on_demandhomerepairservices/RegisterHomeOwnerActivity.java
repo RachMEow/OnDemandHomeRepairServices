e67951cdc490c72b34538d2fe85d68d0573b6254
Sike Yin <byin052@uottawa.ca>
2018-11-14 22:05:44 -0500

    cs

--- app/src/main/java/com/ondemandhomerepairservices/on_demandhomerepairservices/RegisterHomeOwnerActivity.java
+++ app/src/main/java/com/ondemandhomerepairservices/on_demandhomerepairservices/RegisterHomeOwnerActivity.java
@@ -1,14 +1,19 @@
 package com.ondemandhomerepairservices.on_demandhomerepairservices;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class RegisterHomeOwnerActivity extends AppCompatActivity {
 
@@ -27,6 +32,110 @@ public class RegisterHomeOwnerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_owner_register);
+
+        firstName = (EditText) findViewById(R.id.editTextFirstName);
+        lastName = (EditText) findViewById(R.id.editTextLastName);
+        address = (EditText) findViewById(R.id.editTextAddress);
+        postCode = (EditText) findViewById(R.id.editTextPostCode);
+
+        databaseAccounts = FirebaseDatabase.getInstance().getReference("accounts");
+
+        btnclear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reset();
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // String id;
+                String firstN = trimString(firstName.getText().toString());
+                String lastN = trimString(lastName.getText().toString());
+                String addr = trimString(address.getText().toString());
+                String postC = trimString(postCode.getText().toString());
+
+                Pattern p1 = Pattern.compile("[^-," +
+                        "^a-zA-Z_0-9" + "^\\t]", Pattern.CASE_INSENSITIVE);
+                Pattern p2 = Pattern.compile("[^a-zA-z ]", Pattern.CASE_INSENSITIVE);
+                Pattern p3 = Pattern.compile("[^a-zA-Z0-9 \\t]");
+
+                boolean fn = p2.matcher(firstN).find();
+                boolean ln = p2.matcher(lastN).find();
+                boolean ad = p1.matcher(addr).find();
+                boolean pc = p3.matcher(postC).find();
+
+
+                //Validate
+
+                if(fn) {
+                    Toast.makeText(getApplicationContext(), "First name can only contains letter", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(ln){
+                    Toast.makeText(getApplicationContext(), "Last name can only contains letter", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(ad){
+                    Toast.makeText(getApplicationContext(), "Invalid address", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(pc || postC.length()<6){
+                    Toast.makeText(getApplicationContext(), "Invalid Post Code", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(firstN)) {
+                    Toast.makeText( getApplicationContext(), "Enter First Name", Toast.LENGTH_SHORT ).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(lastN)){
+                    Toast.makeText(getApplicationContext(), "Enter Last Name", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(addr)){
+                    Toast.makeText(getApplicationContext(), "Enter Address", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(postC)){
+                    Toast.makeText(getApplicationContext(), "Enter Post Code", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+              //  String id = databaseAccounts.push().getKey();
+
+                //create an account object
+              //  account = new Account(id, firstN, lastN, addr, pc, role);
+
+                //saving the account
+               // databaseAccounts.child(id).setValue(account);
+
+                finish();
+                startActivity(new Intent(RegisterHomeOwnerActivity.this, LoginUser.class));
+
+
+            }
+        });
+    }
+
+    public void reset(){
+        firstName.getText().clear();
+        lastName.getText().clear();
+        address.getText().clear();
+        postCode.getText().clear();
+        return;
+
+    }
+
+    public String trimString(String e) {
+        return e.trim();
     }
 
 
