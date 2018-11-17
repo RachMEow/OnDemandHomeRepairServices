0c0e587eae456e99e32bd4fb0d50d38884fef6c5
JiaruiSun <jiarui025@gmail.com>
2018-11-16 22:15:01 -0500

    register main activity finished

--- app/src/main/java/com/ondemandhomerepairservices/on_demandhomerepairservices/RegisterMainActivity.java
+++ app/src/main/java/com/ondemandhomerepairservices/on_demandhomerepairservices/RegisterMainActivity.java
@@ -1,13 +1,43 @@
 package com.ondemandhomerepairservices.on_demandhomerepairservices;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterMainActivity extends AppCompatActivity {
+
+    Button btnAdmin, btnServiceProvider, btnHomeOwner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_main);
+
+        btnAdmin = (Button)findViewById(R.id.buttonAdmin);
+        btnServiceProvider = (Button)findViewById(R.id.buttonServiceProvider);
+        btnHomeOwner = (Button)findViewById(R.id.buttonHomeOwner);
+
+        btnAdmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterMainActivity.this, RegisterAdminActivity.class));
+            }
+        });
+
+        btnServiceProvider.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterMainActivity.this, RegisterServiceProviderActivity.class));
+            }
+        });
+
+        btnHomeOwner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterMainActivity.this, RegisterHomeOwnerActivity.class));
+            }
+        });
     }
 }
