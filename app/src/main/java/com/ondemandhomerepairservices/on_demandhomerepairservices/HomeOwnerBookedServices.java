6a9e70556f8de5eea925f5870dafc3d9830f2869
Fluffy_Peppa <michelle1120lee@hotmail.com>
2018-11-22 23:05:47 -0500

    3UI button done

--- app/src/main/java/com/ondemandhomerepairservices/on_demandhomerepairservices/HomeOwnerBookedServices.java
+++ app/src/main/java/com/ondemandhomerepairservices/on_demandhomerepairservices/HomeOwnerBookedServices.java
@@ -2,12 +2,23 @@ package com.ondemandhomerepairservices.on_demandhomerepairservices;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeOwnerBookedServices extends AppCompatActivity {
+    Button btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_home_owner_booked_service );
+
+        btnBack = (Button) findViewById(R.id.buttonBack);
+        btnBack.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        } );
     }
 }
