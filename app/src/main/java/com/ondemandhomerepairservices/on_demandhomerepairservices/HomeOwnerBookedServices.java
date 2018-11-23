4605d23a52381e5e03306f505ae03613a9ecd958
Fluffy_Peppa <michelle1120lee@hotmail.com>
2018-11-22 22:47:43 -0500

    login ho ui update and ho booked service ui and ho service list ui done

---
+++ app/src/main/java/com/ondemandhomerepairservices/on_demandhomerepairservices/HomeOwnerBookedServices.java
@@ -0,0 +1,13 @@
+package com.ondemandhomerepairservices.on_demandhomerepairservices;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeOwnerBookedServices extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_home_owner_booked_service );
+    }
+}
