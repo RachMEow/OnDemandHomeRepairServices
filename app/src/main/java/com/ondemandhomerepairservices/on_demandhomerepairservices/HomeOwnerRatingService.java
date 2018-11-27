d9e79cce0162973b1674659ad70d8e2ca310bf19
Fluffy_Peppa <michelle1120lee@hotmail.com>
2018-11-27 16:24:24 -0500

    updated rating and spinner

--- app/src/main/java/com/ondemandhomerepairservices/on_demandhomerepairservices/HomeOwnerRatingService.java
+++ app/src/main/java/com/ondemandhomerepairservices/on_demandhomerepairservices/HomeOwnerRatingService.java
@@ -1,4 +1,4 @@
-package com.ondemandhomerepairservices.on_demandhomerepairservices.serviceProvider;
+package com.ondemandhomerepairservices.on_demandhomerepairservices;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
--- app/src/main/java/com/ondemandhomerepairservices/on_demandhomerepairservices/HomeOwnerSearchServiceProvider.java
+++ app/src/main/java/com/ondemandhomerepairservices/on_demandhomerepairservices/HomeOwnerSearchServiceProvider.java
@@ -3,16 +3,36 @@ package com.ondemandhomerepairservices.on_demandhomerepairservices;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+
+import com.ondemandhomerepairservices.on_demandhomerepairservices.serviceProvider.DayOfWeek;
 
 public class HomeOwnerSearchServiceProvider extends AppCompatActivity {
 
-    Button btnBack;
+    Button btnBack,btnSearch1,btnSearch2,btnSearch3;
+    private DayOfWeek day;
+    private Spinner spinnerDay;
+    private EditText editTextFrom, editTextTo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_home_owner_search_service_provider );
+
+        editTextFrom = (EditText) findViewById(R.id.editTextTimeFrom);
+        editTextTo = (EditText) findViewById(R.id.editTextTimeTo);
+
+        spinnerDay = (Spinner) findViewById(R.id.spinnerDay);
+
+        ArrayAdapter<DayOfWeek> adapterDay = new ArrayAdapter<DayOfWeek>(this, android.R.layout.simple_spinner_dropdown_item, DayOfWeek.values());
+        spinnerDay.setAdapter(adapterDay);
+        day = (DayOfWeek) spinnerDay.getItemAtPosition(spinnerDay.getSelectedItemPosition());
+
+
 
         btnBack = (Button) findViewById(R.id.buttonBack);
         btnBack.setOnClickListener( new View.OnClickListener() {
